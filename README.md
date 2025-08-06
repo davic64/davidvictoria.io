b034acba7d7e24fe9909e57bda0bcc7bc9e1f87e
David Victoria <davidvictoria64@gmail.com>
2025-08-06 16:34:10 -0600

    🎉 initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# davidvictoria.io
---
+++ src/config/siteConfig.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  itemsNav: [
+    { label: "Inicio", href: "/" },
+    { label: "Estructura de Datos", href: "/estructura-de-datos" },
+    { label: "Algoritmos", href: "/algoritmos" },
+  ],
+};
