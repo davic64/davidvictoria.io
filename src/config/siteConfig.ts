fe939bb8a5d3eceaeec66d6cabe9852ebc978279
David Victoria <davidvictoria64@gmail.com>
2025-08-07 16:53:31 -0600

    📱 feat: implement responsive design across components

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -1,7 +1,3 @@
 export const siteConfig = {
-  itemsNav: [
-    { label: "Inicio", href: "/" },
-    { label: "Estructura de Datos", href: "/estructura-de-datos" },
-    { label: "Algoritmos", href: "/algoritmos" },
-  ],
+  itemsNav: [{ label: "Inicio", href: "/" }],
 };
